8f3fcee027da11cde4e3d6fbb80fb08c12686f70
dboldy <70780505+dboldy@users.noreply.github.com>
2020-11-14 12:31:18 -0500

    FirstClass1.java

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Repo_Selenium
+Hello World
---
+++ src/test/java/package1/FirstClass1.java
@@ -0,0 +1,32 @@
+package package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static java.lang.Thread.*;
+
+
+public class FirstClass1 {
+
+ @Test
+  public void lunchPage1() {
+System.setProperty("webdriver.chrome.driver", "./DriverExe/chromedriver");
+
+     WebDriver driver = new ChromeDriver();
+
+     driver.get("https://facebook.com");
+
+driver.navigate().to("https://www.facebook.com");
+driver.manage().window().fullscreen();
+String pageTitle = driver.getTitle();
+     System.out.println("Page Title" + pageTitle);
+     try {
+         sleep(7000);
+     } catch (InterruptedException e) {
+         e.printStackTrace();
+     }
+     driver.quit();
+
+ }
+}
